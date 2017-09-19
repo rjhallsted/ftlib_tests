03cb8f7024ac63de2a6c8150209695cbfd6932a9
RJ Hallsted <rhallste@student.42.us.org>
2017-09-18 21:14:11 -0700

    some p1 and p2 progress

--- test.sh
+++ test.sh
@@ -6,10 +6,12 @@
 #    By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/09/18 14:41:35 by rhallste          #+#    #+#              #
-#    Updated: 2017/09/18 17:11:58 by rhallste         ###   ########.fr        #
+#    Updated: 2017/09/18 21:05:16 by rhallste         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 make -C ../libft/
-gcc -Wall -Werror -Wextra ../libft/libft.a main.c
-./a.out
+gcc -Wall -Werror -Wextra ../libft/libft.a p1.c -o p1
+gcc -Wall -Werror -Wextra ../libft/libft.a p2.c -o p2
+./p1
+./p2
