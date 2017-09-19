198d49d1d58e37d10e5ea04de9efec62e60f3d49
RJ Hallsted <rhallste@student.42.us.org>
2017-09-19 14:54:49 -0700

    tests for ft_strlen, ft_strcmp, ft_strncmp

--- p1.c
+++ p1.c
@@ -6,13 +6,14 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 21:01:27 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/19 11:36:30 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/09/19 14:52:35 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #include "../libft/libft.h"
 
 void int_test(int (*test)(int), int (*real)(int), int input, int nb)
@@ -30,11 +31,53 @@ void char_test(int (*test)(int), int (*real)(int), int input, int nb)
 	int_test(test, real, input, nb);
 }
 
+void str_to_int_test(size_t (*test)(const char *), size_t (*real)(const char *), char * input, int nb)
+{
+	int tr;
+	int rr;
+
+	tr = test(input);
+	rr = real(input);
+	printf("%d) %d == %d --> %d\n", nb, tr, rr, tr == rr);
+}
+
 int main(void)
 {
 	int i;
 
 	i = 1;
+
+	printf("\n===ft_strlen===\n");
+	str_to_int_test(&ft_strlen, &strlen, "Testing", i);
+	str_to_int_test(&ft_strlen, &strlen, "", i);
+	str_to_int_test(&ft_strlen, &strlen, "Redadsadf af", i);
+
+	char *s1;
+	char *s2;
+	printf("\n===ft_strcmp===\n");
+	s1 = "red";
+	s2 = "blue";
+	printf("%d) %d == %d --. %d\n", i++, ft_strcmp(s1, s2), strcmp(s1, s2), ft_strcmp(s1, s2) == strcmp(s1, s2));
+	s1 = "";
+	s2 = "blue";
+	printf("%d) %d == %d --. %d\n", i++, ft_strcmp(s1, s2), strcmp(s1, s2), ft_strcmp(s1, s2) == strcmp(s1, s2));
+
+	int n;
+	printf("\n===ft_strncmp===\n");
+	s1 = "red";
+	s2 = "blue";
+	n = 2;
+	printf("%d) %d == %d --. %d\n", i++, ft_strncmp(s1, s2, n), strncmp(s1, s2, n), ft_strncmp(s1, s2, n) == strncmp(s1, s2, n));
+	s1 = "";
+	s2 = "blue";
+	n = 3;
+	printf("%d) %d == %d --. %d\n", i++, ft_strncmp(s1, s2, n), strncmp(s1, s2, n), ft_strncmp(s1, s2, n) == strncmp(s1, s2, n));
+	s1 = "red";
+	s2 = "blue";
+	n = 0;
+	printf("%d) %d == %d --. %d\n", i++, ft_strncmp(s1, s2, n), strncmp(s1, s2, n), ft_strncmp(s1, s2, n) == strncmp(s1, s2, n));
+
+
 	printf("\n===ft_atoi===\n");
 	printf("%d) %d == %d --> %d\n", i++, ft_atoi("-2147483648"), atoi("-2147483648"), ft_atoi("-2147483648") == atoi("-2147483648"));
 	printf("%d) %d == %d --> %d\n", i++, ft_atoi("2147483647"), atoi("2147483647"), ft_atoi("2147483647") == atoi("2147483647"));
--- test.sh
+++ test.sh
@@ -6,12 +6,14 @@
 #    By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/09/18 14:41:35 by rhallste          #+#    #+#              #
-#    Updated: 2017/09/18 21:05:16 by rhallste         ###   ########.fr        #
+#    Updated: 2017/09/19 14:54:17 by rhallste         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
 
 make -C ../libft/
 gcc -Wall -Werror -Wextra ../libft/libft.a p1.c -o p1
 gcc -Wall -Werror -Wextra ../libft/libft.a p2.c -o p2
+clear
 ./p1
 ./p2
