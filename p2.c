c7ea9868a70dab1ae8c1997563a582c920d7c86b
RJ Hallsted <rhallste@student.42.us.org>
2017-09-22 13:57:00 -0700

    Tests for part 2 fucs (some)

--- p2.c
+++ p2.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+	/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   p2.c                                               :+:      :+:    :+:   */
@@ -6,7 +6,7 @@
 /*   By: rhallste <rhallste@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 16:20:06 by rhallste          #+#    #+#             */
-/*   Updated: 2017/09/19 16:42:54 by rhallste         ###   ########.fr       */
+/*   Updated: 2017/09/22 13:56:30 by rhallste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,43 @@
 #include <string.h>
 #include "../libft/libft.h"
 
+void print_zvalues(char *s, size_t len)
+{
+	size_t i;
+
+	i = 0;
+	while (i < len)
+		printf("%c", s[i++] + '0');
+	printf("\n");
+}
+
+void shiftchar(char *c)
+{
+	*c += 1;
+}
+
+void shiftchar_byindex(unsigned int index, char *c)
+{
+	*c += index;
+}
+
+char cshiftchar(char c)
+{
+	return (c + 1);
+}
+
+char cshiftchar_byindex(unsigned int index, char c)
+{
+	return (c + index);
+}
+
+
 int main(void)
 {
+	printf("\n\n============================\n");
+	printf("====     PART 2     ========\n");
+	printf("============================\n");
+
 	int len;
 	char *mem;
 	printf("\n===ft_memalloc===\n");
@@ -25,6 +60,7 @@ int main(void)
 	while (len > 0)
 		printf("%c", mem[len--] + '0');
 	printf("\n");
+	free(mem);	
 
 	void **p1;
 	void *p2;
@@ -33,13 +69,47 @@ int main(void)
 	printf("\n===ft_memdel===\n");
 	ft_memdel(p1);
 	printf("%p\n", p2);
+	free (p2);
+	p1 = NULL;
 
+	printf("\n===ft_strnew===\n");
+	printf("000000: ");
+	print_zvalues(ft_strnew(5), 6);
+
+	printf("\n===ft_strdel===\n");
+	char *ft_str;
+	ft_str = ft_strnew(5);
+	ft_strdel(&ft_str);
+	printf("(null): %s\n", ft_str);
+
+	printf("\n===ft_striter===\n");
+	ft_str = strdup("aaaaaa");
+	ft_striter(ft_str, &shiftchar);
+	printf("bbbbbb: %s\n", ft_str);
+	free(ft_str);
+
+	printf("\n===ft_striteri===\n");
+	ft_str = strdup("aaaaaa");
+	ft_striteri(ft_str, &shiftchar_byindex);
+	printf("abcdef: %s\n", ft_str);
+	free(ft_str);
+
+	printf("\n===ft_strmap===\n");
+	ft_str = ft_strmap("aaaaaa", &cshiftchar);
+	printf("bbbbbb: %s\n", ft_str);
+	free(ft_str);
+
+	printf("\n===ft_strimapi===\n");
+	ft_str = ft_strmapi("aaaaaa", &cshiftchar_byindex);
+	printf("abcdef: %s\n", ft_str);
+	free(ft_str);
 
 	printf("\n===ft_strclr=== (should be empty lines)\n");
 	char *s1 = (char *)malloc(sizeof(char) * 4);
 	s1 = strcpy(s1, "red");
 	ft_strclr(s1);
 	printf("%s\n", s1);
+	free(s1);
 
 	printf("\n===ft_putchar===\n");
 	ft_putchar('a');
